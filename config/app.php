e662f58a790fa798ed1e83b9899182cdc3e90c11
Diarmur <silotia.mathis@gmail.com>
2024-01-16 17:32:24 +0100

    change http

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://localhost'),
 
     'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
 
